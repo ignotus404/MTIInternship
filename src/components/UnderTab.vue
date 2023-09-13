70d8db26b91de6a5a9c705d0422baae73dcfb4e3
Takaya Shintani <ddd20020817@gmail.com>
2023-09-13 07:11:57 +0000

    下タブの名前を変えたりするだけ

---
+++ src/components/UnderTab.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tab-container">
+    <div
+      v-for="item in tabs"
+      :key="item"
+      class="tab"
+      @click="selectTab(item)"
+    >
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabs: [
+        { name: "ホーム", path: "/" },
+        { name: "投稿", path: "/post" },
+        { name: "日記", path: "/diary" },
+        { name: "設定", path: "/settings" }
+      ]
+    };
+  },
+  methods: {
+    navigateTo(path) {
+      this.$router.push(path);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tab-container {
+  display: flex;
+  justify-content: space-between;
+  height: 30px;
+  
+}
+
+.tab {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.tab:hover {
+  background-color: #ddd;
+}
+</style>
